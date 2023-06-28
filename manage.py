98c517f4037579e0174414d05147b45f4b7d7ecb
leejinhyeok <tool7831@gmail.com>
2023-06-28 17:39:45 +0900

    add nested run (delete run.py) run.py 지우고 manage.py로 통합해서 이제 manage.py랑 train.py만 있으면 돌아갑니다.

--- manage.py
+++ manage.py
@@ -22,11 +22,11 @@ from torch.utils.data import DataLoader
 from mlflow.models.signature import infer_signature
 from sklearn.model_selection import KFold
 
-import run 
+import train
 import argparse
 from mlflow.models.signature import ModelSignature
 from mlflow.types.schema import Schema, TensorSpec
-import seaborn as sns
+from datetime import datetime as dt
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
@@ -59,6 +59,7 @@ class CreateImageDataset(Dataset):
         
         label = self.img_labels.iloc[idx]['grade_encode']
         grade = self.img_labels.iloc[idx]['grade']
+        name = self.img_labels.iloc[idx]['file_name']
         img_folder = f'grade_{grade}'
         img_path = os.path.join(self.img_dir, img_folder)
         img_path = os.path.join(img_path, self.img_labels.iloc[idx, 0])
@@ -67,8 +68,7 @@ class CreateImageDataset(Dataset):
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
-        return image, label
-
+        return image, label, name
 
 
 
@@ -120,7 +120,7 @@ train_label_set['grade_encode'] = train_label_set['grade'].apply(grade_encoding)
 val_label_set['grade_encode'] = val_label_set['grade'].apply(grade_encoding)
 
 #Define kfold
-fold = args.kfold
+kfold = args.kfold
 
 #Define input transform
 transformation = transforms.Compose([
@@ -134,7 +134,7 @@ transforms.ToTensor(),
 train_dataset = CreateImageDataset(train_label_set, trainpath, transform=transformation)
 valid_dataset = CreateImageDataset(val_label_set, valpath, transform=transformation)
 dataset = torch.utils.data.ConcatDataset([train_dataset, valid_dataset])
-splits = KFold(n_splits = fold, shuffle = True, random_state =42)
+splits = KFold(n_splits = kfold, shuffle = True, random_state =42)
 
 #Define hyperparameters
 batch_size = args.batch_size
@@ -154,66 +154,103 @@ logged_model = args.logged_model #'runs:/523f68657d884879844be1c409bd96c0/best'
 
 experiment_name = args.model_type
 run_name = args.run_name
-params_vit = {
-    'num_epochs':epochs,
-    'batch_size':batch_size,
-    'image_size':image_size,
-    'lr':lr,
-    'optimizer':None,
-    'loss_func':None,
-    'train_dl':None,
-    'val_dl':None,
-    'sanity_check':sanity,
-    'lr_scheduler':None,
-    'log_epoch':log_epoch,
-    'model_name':model_name,
-    'pretrained':pretrained,
-    'load_run':load_run,
-    'logged_model':logged_model,
-    'fold':0,
-    'experiment_name':experiment_name,
-    'run_name':run_name,
-    'signature':None,
-}
 
 
-for fold, (train_idx,val_idx) in enumerate(splits.split(np.arange(len(dataset)))):            
-    print('Fold {}'.format(fold + 1))
 
-    if load_run == True:
-        model = mlflow.pytorch.load_model(logged_model)
+mlflow.set_experiment(experiment_name)
+now = dt.now()
+date_time_string = now.strftime("%Y-%m-%d %H:%M:%S")
+
+train_acc_sum = np.zeros(epochs)
+val_acc_sum = np.zeros(epochs)
+train_loss_sum = np.zeros(epochs)
+val_loss_sum = np.zeros(epochs)
+if run_name == None:
+    run_name = f"{experiment_name}: " + str(date_time_string)
+else:
+    run_name = f"{run_name}: " + str(date_time_string)
+
+# Start running
+with mlflow.start_run(run_name=run_name) as parent_run:
+    print(parent_run.info.run_id)
+    if load_run == False:
+        mlflow.log_param("model_name", model_name)
+        mlflow.log_param('pretrained', pretrained)
     else:
-        model = timm.create_model(model_name, pretrained=pretrained, num_classes=num_classes)
+        mlflow.log_param("model_name", logged_model)
+        mlflow.log_param("pretrained", True)
 
+    mlflow.log_param("num_epochs", epochs)
+    mlflow.log_param("learning_rate", lr)
+    mlflow.log_param('batch_size', batch_size)
+    mlflow.log_param("image_size", image_size)
 
-    model = model.to(device)
     loss_func = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr = lr)
-    scheduler = ReduceLROnPlateau(optimizer, patience = 2, factor = args.factor, threshold = args.threshold)
+    mlflow.log_param("loss_func", loss_func)
 
-    train_sampler = SubsetRandomSampler(train_idx) 
-    test_sampler = SubsetRandomSampler(val_idx)
-    train_dl = DataLoader(dataset, batch_size=batch_size, sampler=train_sampler, num_workers=num_workers, pin_memory=True)
-    val_dl = DataLoader(dataset, batch_size=batch_size, sampler=test_sampler, num_workers=num_workers, pin_memory=True)
+    for fold, (train_idx,val_idx) in enumerate(splits.split(np.arange(len(dataset)))):            
+        print('Fold {}'.format(fold + 1))
 
-    input_schema = Schema([TensorSpec(np.dtype(np.float32),shape=(image_size,image_size))])
-    output_schema = Schema([TensorSpec(np.dtype(np.float32), (1, num_classes))])
-    signature = ModelSignature(inputs=input_schema, outputs=output_schema)
+        if load_run == True:
+            model = mlflow.pytorch.load_model(logged_model)
+        else:
+            model = timm.create_model(model_name, pretrained=pretrained, num_classes=num_classes)
 
-    # train_dl = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    # val_dl = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+        model = model.to(device)
+        optimizer = optim.Adam(model.parameters(), lr = lr)
+        scheduler = ReduceLROnPlateau(optimizer, patience = 2, factor = args.factor, threshold = args.threshold)
 
-    params_vit['optimizer']=optimizer
-    params_vit['lr_scheduler']=scheduler
-    params_vit['loss_func']=loss_func
-    params_vit['train_dl']=train_dl
-    params_vit['val_dl']=val_dl
-    params_vit['fold']=fold
-    params_vit['signature']=signature
-    run.run(model, params_vit)
-    model.cpu()
-    del model
-    gc.collect()
+        train_sampler = SubsetRandomSampler(train_idx) 
+        test_sampler = SubsetRandomSampler(val_idx)
+        train_dl = DataLoader(dataset, batch_size=batch_size, sampler=train_sampler, num_workers=num_workers, pin_memory=True)
+        val_dl = DataLoader(dataset, batch_size=batch_size, sampler=test_sampler, num_workers=num_workers, pin_memory=True)
 
+        input_schema = Schema([TensorSpec(np.dtype(np.float32),shape=(image_size,image_size))])
+        output_schema = Schema([TensorSpec(np.dtype(np.float32), (1, num_classes))])
+        signature = ModelSignature(inputs=input_schema, outputs=output_schema)
+
+        # train_dl = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+        # val_dl = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+        params_train = {
+        'num_epochs':epochs,
+        'optimizer':optimizer,
+        'loss_func':loss_func,
+        'train_dl':train_dl,
+        'val_dl':val_dl,
+        'sanity_check':sanity,
+        'lr_scheduler':scheduler,
+        'log_epoch':log_epoch,
+        'fold':fold,
+        'signature':signature,
+        }
+
+        with mlflow.start_run(run_name=str(fold+1), nested=True) as run:
+            model, train_acc, val_acc, train_loss, val_loss = train.training(model, params_train)
+            train_acc_sum += train_acc
+            val_acc_sum += val_acc
+            train_loss_sum += train_loss
+            val_loss_sum += val_loss
+
+            #plot the curves
+            plt.plot(train_acc, label = 'train_acc')
+            plt.plot(val_acc, label = 'val_acc')
+            plt.plot(train_loss, label = 'train_loss')
+            plt.plot(val_loss, label = 'val_loss')
+            plt.legend()
+            plt.title('Accuracy and Loss Plots')
+            figure = plt.figure()
+            mlflow.log_figure(figure, "Graph_"+str(log_epoch)+'_'+str(fold+1)+'.jpg')
+            plt.clf()
+
+
+        model.cpu()
+        del model
+        gc.collect()
+    for i in range(epochs):
+        mlflow.log_metric("train loss", train_loss_sum[i] / kfold, i)
+        mlflow.log_metric("train accuracy", train_acc_sum[i] / kfold , i)
+        mlflow.log_metric("val loss", val_loss_sum[i] / kfold, i)
+        mlflow.log_metric("val accuracy", val_acc_sum[i] / kfold, i)
 
 torch.cuda.empty_cache()
--- train.py
+++ train.py
@@ -3,11 +3,13 @@ import mlflow
 from tqdm import tqdm
 import CM as cm
 import numpy as np
+import pandas as pd
+import os
 import sklearn
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # calculate the loss per epochs
-def loss_epoch(model, loss_func, dataset_dl, epoch, fold, sanity_check=False, opt=None):
+def loss_epoch(model, loss_func, dataset_dl, epoch, fold, sanity_check=False, opt=None, df=None):
     running_loss = 0.0
     running_metrics = 0.0
     len_data = len(dataset_dl.sampler)
@@ -15,7 +17,7 @@ def loss_epoch(model, loss_func, dataset_dl, epoch, fold, sanity_check=False, op
     conf_label = []
     conf_pred = []
 
-    for xb, yb in tqdm(dataset_dl):
+    for xb, yb, name_b in tqdm(dataset_dl):
         xb = xb.to(device)
         yb = yb.to(device)
         output = model(xb)
@@ -28,10 +30,25 @@ def loss_epoch(model, loss_func, dataset_dl, epoch, fold, sanity_check=False, op
             opt.zero_grad()
             loss_b.backward()
             opt.step()
+        
+        if opt is None:
+            # Confusion Matrix에 쓰일 data append하는 부분
             predictions_conv = pred_b.cpu().numpy()
             labels_conv = yb.cpu().numpy()
             conf_pred.append(predictions_conv)
             conf_label.append(labels_conv)
+
+            if df is not None:
+                # 틀린 이미지 csv로 저장하는 부분
+                index = torch.nonzero((pred_b != yb)).squeeze().cpu().tolist()
+                if not isinstance(index, list):
+                    index = [index]  # index가 단일 값인 경우에 리스트로 변환하여 처리
+                scores = scores.cpu().numpy()
+                output = list(output.detach().cpu().numpy())
+                name_b = list(name_b)
+                for i in index:
+                    new_row = pd.DataFrame({'file_name':name_b[i], 'output':output[i], 'score':scores[i], 'prediction':predictions_conv[i]})
+                    df = pd.concat([df,new_row], ignore_index=True)
 
 
         if metric_b is not None:
@@ -40,17 +57,17 @@ def loss_epoch(model, loss_func, dataset_dl, epoch, fold, sanity_check=False, op
         if sanity_check is True:
             break
 
-    if opt is not None:   
+    # Validation 일 때 Confusion Matrix 그리는 부분
+    if opt is None:   
         new_pred = np.concatenate(conf_pred)
         new_label = np.concatenate(conf_label)
-
         con_mat=sklearn.metrics.confusion_matrix(new_label, new_pred)
         CM=cm.SaveCM(con_mat,epoch)
         CM.save_plot(fold)
 
     loss = running_loss / len_data
     metric = running_metrics / len_data
-    return loss, metric
+    return loss, metric, df
 
 
 def training(model, params):
@@ -67,11 +84,16 @@ def training(model, params):
 
     train_loss, val_loss, train_metric, val_metric =[], [], [], []
     best_acc = 0
-
     for epoch in tqdm(range(num_epochs)):
+
+        if epoch == num_epochs - 1:
+            df = pd.DataFrame(columns=['file_name', 'output', 'score', 'prediction'])
+        else:
+            df = None
+
         #training
         model.train()
-        loss, metric = loss_epoch(model, loss_func, train_dl, epoch, fold+1, sanity, optimizer)
+        loss, metric, _ = loss_epoch(model, loss_func, train_dl, epoch, fold+1, sanity, optimizer)
         mlflow.log_metric("train loss", loss, epoch)
         mlflow.log_metric("train accuracy", metric, epoch)
         train_loss.append(loss)
@@ -80,7 +102,7 @@ def training(model, params):
         #validation
         model.eval()
         with torch.no_grad():
-            loss, metric = loss_epoch(model, loss_func, val_dl, epoch, fold+1, sanity)
+            loss, metric, df = loss_epoch(model, loss_func, val_dl, epoch, fold+1, sanity, df=df)
         mlflow.log_metric("val loss", loss, epoch)
         mlflow.log_metric("val accuracy", metric, epoch)
         val_loss.append(loss)
@@ -93,9 +115,13 @@ def training(model, params):
         #saving best model
         if val_metric[-1]>best_acc:
             best_acc = val_metric[-1]
-            mlflow.set_tag("best", f'best at fold {fold+1}, epoch {epoch}')
+            mlflow.set_tag("best", f'best at epoch {epoch}')
             mlflow.pytorch.log_model(model, f"best", signature=signature)
         print('The Validation Loss is {} and the validation accuracy is {}'.format(val_loss[-1],val_metric[-1]))
         print('The Training Loss is {} and the training accuracy is {}'.format(train_loss[-1],train_metric[-1]))
 
+    if not os.path.exists('temp'):
+        os.mkdir('temp')
+    df.to_csv('temp/incorrect_data.csv')
+    mlflow.log_artifact('temp/incorrect_data.csv')
     return model, train_metric, val_metric, train_loss, val_loss
