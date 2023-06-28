1f9141cf95ca9c9b6cefe96bee41c78218c5ba7f
KSJ <seungjune06@naver.com>
2023-06-28 17:53:32 +0900

    순서는 내가 바꾼다.

--- train.py
+++ train.py
@@ -8,6 +8,63 @@ import os
 import sklearn
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+def training(model, params):
+    num_epochs=params['num_epochs']
+    loss_func=params['loss_func']
+    optimizer=params['optimizer']
+    scheduler=params['lr_scheduler']
+    log_epoch=params['log_epoch']
+    train_dl=params['train_dl']
+    val_dl=params['val_dl']
+    fold=params['fold']
+    sanity=params['sanity_check']
+    signature=params['signature']
+
+    train_loss, val_loss, train_metric, val_metric =[], [], [], []
+    best_acc = 0
+    for epoch in tqdm(range(num_epochs)):
+
+        if epoch == num_epochs - 1:
+            df = pd.DataFrame(columns=['file_name', 'output', 'score', 'prediction'])
+        else:
+            df = None
+
+        #training
+        model.train()
+        loss, metric, _ = loss_epoch(model, loss_func, train_dl, epoch, fold+1, sanity, optimizer)
+        mlflow.log_metric("train loss", loss, epoch)
+        mlflow.log_metric("train accuracy", metric, epoch)
+        train_loss.append(loss)
+        train_metric.append(metric)
+
+        #validation
+        model.eval()
+        with torch.no_grad():
+            loss, metric, df = loss_epoch(model, loss_func, val_dl, epoch, fold+1, sanity, df=df)
+        mlflow.log_metric("val loss", loss, epoch)
+        mlflow.log_metric("val accuracy", metric, epoch)
+        val_loss.append(loss)
+        val_metric.append(metric)
+        scheduler.step(val_loss[-1])
+
+        if epoch % log_epoch == log_epoch-1:
+            mlflow.pytorch.log_model(model, f'model_fold_{fold+1}_epoch_{epoch}', signature=signature)
+            
+        #saving best model
+        if val_metric[-1]>best_acc:
+            best_acc = val_metric[-1]
+            mlflow.set_tag("best", f'best at epoch {epoch}')
+            mlflow.pytorch.log_model(model, f"best", signature=signature)
+        print('The Validation Loss is {} and the validation accuracy is {}'.format(val_loss[-1],val_metric[-1]))
+        print('The Training Loss is {} and the training accuracy is {}'.format(train_loss[-1],train_metric[-1]))
+
+    if not os.path.exists('temp'):
+        os.mkdir('temp')
+    df.to_csv('temp/incorrect_data.csv')
+    mlflow.log_artifact('temp/incorrect_data.csv')
+    return model, train_metric, val_metric, train_loss, val_loss
+
+
 # calculate the loss per epochs
 def loss_epoch(model, loss_func, dataset_dl, epoch, fold, sanity_check=False, opt=None, df=None):
     running_loss = 0.0
@@ -69,59 +126,3 @@ def loss_epoch(model, loss_func, dataset_dl, epoch, fold, sanity_check=False, op
     metric = running_metrics / len_data
     return loss, metric, df
 
-
-def training(model, params):
-    num_epochs=params['num_epochs']
-    loss_func=params['loss_func']
-    optimizer=params['optimizer']
-    scheduler=params['lr_scheduler']
-    log_epoch=params['log_epoch']
-    train_dl=params['train_dl']
-    val_dl=params['val_dl']
-    fold=params['fold']
-    sanity=params['sanity_check']
-    signature=params['signature']
-
-    train_loss, val_loss, train_metric, val_metric =[], [], [], []
-    best_acc = 0
-    for epoch in tqdm(range(num_epochs)):
-
-        if epoch == num_epochs - 1:
-            df = pd.DataFrame(columns=['file_name', 'output', 'score', 'prediction'])
-        else:
-            df = None
-
-        #training
-        model.train()
-        loss, metric, _ = loss_epoch(model, loss_func, train_dl, epoch, fold+1, sanity, optimizer)
-        mlflow.log_metric("train loss", loss, epoch)
-        mlflow.log_metric("train accuracy", metric, epoch)
-        train_loss.append(loss)
-        train_metric.append(metric)
-
-        #validation
-        model.eval()
-        with torch.no_grad():
-            loss, metric, df = loss_epoch(model, loss_func, val_dl, epoch, fold+1, sanity, df=df)
-        mlflow.log_metric("val loss", loss, epoch)
-        mlflow.log_metric("val accuracy", metric, epoch)
-        val_loss.append(loss)
-        val_metric.append(metric)
-        scheduler.step(val_loss[-1])
-
-        if epoch % log_epoch == log_epoch-1:
-            mlflow.pytorch.log_model(model, f'model_fold_{fold+1}_epoch_{epoch}', signature=signature)
-            
-        #saving best model
-        if val_metric[-1]>best_acc:
-            best_acc = val_metric[-1]
-            mlflow.set_tag("best", f'best at epoch {epoch}')
-            mlflow.pytorch.log_model(model, f"best", signature=signature)
-        print('The Validation Loss is {} and the validation accuracy is {}'.format(val_loss[-1],val_metric[-1]))
-        print('The Training Loss is {} and the training accuracy is {}'.format(train_loss[-1],train_metric[-1]))
-
-    if not os.path.exists('temp'):
-        os.mkdir('temp')
-    df.to_csv('temp/incorrect_data.csv')
-    mlflow.log_artifact('temp/incorrect_data.csv')
-    return model, train_metric, val_metric, train_loss, val_loss
