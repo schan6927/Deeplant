962e2b40c960e3f8c4561ed120bc21263e260178
leejinhyeok <tool7831@gmail.com>
2023-06-29 12:50:37 +0900

    incorrect data 랑 label을 merge하는 코드

--- csv_file/incorrect_data.py
+++ csv_file/incorrect_data.py
@@ -2,9 +2,12 @@ import os
 import yaml
 import pandas as pd
 
-mlruns_path = "../mlruns"
-data_label_path = '/home/work/image....'
-label_df = pd.read_csv(data_label_path)
+mlruns_path = "../../mlruns"
+train_label_path = '/home/work/resized_image_datas/image_5class_5000/train_all.csv'
+valid_label_path = '/home/work/resized_image_datas/image_5class_5000/valid_all.csv'
+train_df = pd.read_csv(train_label_path)
+valid_df = pd.read_csv(valid_label_path)
+label_df = pd.concat([train_df,valid_df], ignore_index=True)
 
 def read_yaml_file(file_path):
     with open(file_path, 'r') as file:
@@ -14,24 +17,26 @@ def read_yaml_file(file_path):
         except yaml.YAMLError as e:
             print(f'YAML 파일을 읽는 중 오류가 발생했습니다: {e}')
 
+# mlruns에 있는 모든 run을 돌면서 incorrect_data를 기존 라벨과 merge를 함
 def convert_all():
     for experiment in os.listdir(mlruns_path):
         if not experiment.endswith('.yaml'):
             experiment_path = os.path.join(mlruns_path, experiment)
             convert_experiment(experiment_path)
-        
 
+# expriment안의 모든 runs
 def convert_experiment(experiment_path):
     for run in os.listdir(experiment_path):
         if not run.endswith('.yaml'):
             run_path = os.path.join(experiment_path, run)
             convert_run(run_path)
 
+# 하나의 run
 def convert_run(run_path):
     yaml_path = os.path.join(run_path, "meta.yaml")
     data = read_yaml_file(yaml_path)
     experiment_id = data['experiment_id']
-    run_name = data['run_name']
+    run_id = data['run_id']
 
     artifact_path = os.path.join(run_path,"artifacts")
     csv_path = os.path.join(artifact_path,"incorrect_data.csv")
@@ -45,6 +50,9 @@ def convert_run(run_path):
         os.mkdir(experiment_id)
     merge_df.reset_index(drop=True)
 
-    save_path = experiment_id+'/'+run_name+'.csv'
+    # 기본 저장 장소
+    save_path = experiment_id+'/'+run_id+'.csv'
     merge_df.to_csv(save_path)
 
+# 아래처럼 원하는 함수 직접 선언하고 파일 실행하면 됨
+# convert_experiment("../../mlruns/2/")
