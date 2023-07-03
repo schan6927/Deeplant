3ca3017ec9b3d0642115a6e4183b7f6fbbdd56ec
leejinhyeok <tool7831@gmail.com>
2023-07-03 16:22:56 +0900

    regression 모드 추가. --mode로 classification, regression 선택 가능. train의 training함수는 이름을 바꿔서 classification, regression으로 바뀜 테스트 진행 중.

--- manage.py
+++ manage.py
@@ -56,8 +56,8 @@ class CreateImageDataset(Dataset):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        
-        label = self.img_labels.iloc[idx]['grade_encode']
+
+        label = torch.tensor(self.img_labels.iloc[idx, [3,4]], dtype=torch.float32)
         grade = self.img_labels.iloc[idx]['grade']
         name = self.img_labels.iloc[idx]['file_name']
         img_folder = f'grade_{grade}'
@@ -121,6 +121,41 @@ val_label_set = pd.read_csv(f'{datapath}/valid.csv')
 train_label_set['grade_encode'] = train_label_set['grade'].apply(grade_encoding)
 val_label_set['grade_encode'] = val_label_set['grade'].apply(grade_encoding)
 
+
+# +
+def regression1(x):
+    if x == '1++':
+        return 0.
+    elif x == '1+':
+         return 1.
+    elif x == '1':
+        return 2.
+    elif x == '2':
+        return 3.
+    elif x == '3':
+        return 4.
+    return 0
+
+def regression2(x):
+    if x == '1++':
+        return 4.
+    elif x == '1+':
+         return 3.
+    elif x == '1':
+        return 2.
+    elif x == '2':
+        return 1.
+    elif x == '3':
+        return 0.
+    return 0
+
+train_label_set['regression1'] = train_label_set['grade'].apply(regression1)
+train_label_set['regression2'] = train_label_set['grade'].apply(regression2)
+val_label_set['regression1'] = val_label_set['grade'].apply(regression1)
+val_label_set['regression2'] = val_label_set['grade'].apply(regression2)
+print(train_label_set)
+# -
+
 #Define kfold
 kfold = args.kfold
 
@@ -151,7 +186,7 @@ num_classes = args.num_classes
 sanity = args.sanity
 pretrained = args.pretrained
 model_name = args.model_name
-mode = args.modes
+mode = args.mode
 load_run = args.load_run
 logged_model = args.logged_model
 
@@ -163,10 +198,7 @@ mlflow.set_experiment(experiment_name)
 now = dt.now()
 date_time_string = now.strftime("%Y-%m-%d %H:%M:%S")
 
-train_acc_sum = np.zeros(epochs)
-val_acc_sum = np.zeros(epochs)
-train_loss_sum = np.zeros(epochs)
-val_loss_sum = np.zeros(epochs)
+
 
 if run_name == None:
     run_name = f"{experiment_name}: " + str(date_time_string)
@@ -188,7 +220,19 @@ with mlflow.start_run(run_name=run_name) as parent_run:
     mlflow.log_param('batch_size', batch_size)
     mlflow.log_param("image_size", image_size)
 
-    loss_func = nn.CrossEntropyLoss()
+    if mode == 'classification':
+        loss_func = nn.CrossEntropyLoss()
+        train_acc_sum = np.zeros(epochs)
+        val_acc_sum = np.zeros(epochs)
+        train_loss_sum = np.zeros(epochs)
+        val_loss_sum = np.zeros(epochs)
+    elif mode == 'regression':
+        loss_func = nn.MSELoss()
+        train_acc_sum = np.zeros((epochs,num_classes))
+        val_acc_sum = np.zeros((epochs,num_classes))
+        train_loss_sum = np.zeros(epochs)
+        val_loss_sum = np.zeros(epochs)
+    
     mlflow.log_param("loss_func", loss_func)
 
     for fold, (train_idx,val_idx) in enumerate(splits.split(np.arange(len(dataset)))):            
@@ -226,11 +270,15 @@ with mlflow.start_run(run_name=run_name) as parent_run:
         'log_epoch':log_epoch,
         'fold':fold,
         'signature':signature,
-        'mode':mode,
+        'num_classes':num_classes,
         }
 
         with mlflow.start_run(run_name=str(fold+1), nested=True) as run:
-            model, train_acc, val_acc, train_loss, val_loss = train.training(model, params_train)
+            if mode == 'classification':
+                model, train_acc, val_acc, train_loss, val_loss = train.classification(model, params_train)
+            elif mode == 'regression':
+                model, train_acc, val_acc, train_loss, val_loss = train.regression(model, params_train)
+            
             train_acc_sum += train_acc
             val_acc_sum += val_acc
             train_loss_sum += train_loss
@@ -251,9 +299,13 @@ with mlflow.start_run(run_name=run_name) as parent_run:
         model.cpu()
         del model
         gc.collect()
+    
+    print(train_loss_sum)
+    print(train_acc_sum)
+    print(val_loss_sum)
+    print(train_acc_sum)
     for i in range(epochs):
         mlflow.log_metric("train loss", train_loss_sum[i] / kfold, i)
-        mlflow.log_metric("train accuracy", train_acc_sum[i] / kfold , i)
         if mode == 'classification':
             mlflow.log_metric("train accuracy", train_acc_sum[i] / kfold , i)
         elif mode == 'regression':
--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import os
 import sklearn
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def training(model, params):
+def classification(model, params):
     num_epochs=params['num_epochs']
     loss_func=params['loss_func']
     optimizer=params['optimizer']
@@ -19,7 +19,6 @@ def training(model, params):
     fold=params['fold']
     sanity=params['sanity_check']
     signature=params['signature']
-    mode=params['mode']
 
     train_loss, val_loss, train_metric, val_metric =[], [], [], []
     best_acc = 0
@@ -32,26 +31,18 @@ def training(model, params):
 
         #training
         model.train()
-        loss, metric, _ = loss_epoch(model, loss_func, train_dl, epoch, fold+1, mode, sanity, optimizer)
+        loss, metric, _ = classification_epoch(model, loss_func, train_dl, epoch, fold+1, sanity, optimizer)
         mlflow.log_metric("train loss", loss, epoch)
-        if mode == 'classification':
-            mlflow.log_metric("train accuracy", metric, epoch)
-        elif mode == 'regression':
-            for i in range(len(metric)):
-                mlflow.log_metric(f"train metric {i}",metric[i], epoch)
+        mlflow.log_metric("train accuracy", metric, epoch)
         train_loss.append(loss)
         train_metric.append(metric)
 
         #validation
         model.eval()
         with torch.no_grad():
-            loss, metric, df = loss_epoch(model, loss_func, val_dl, epoch, fold+1, mode, sanity, df=df)
+            loss, metric, df = classification_epoch(model, loss_func, val_dl, epoch, fold+1, sanity, df=df)
         mlflow.log_metric("val loss", loss, epoch)
-        if mode == 'classification':
-            mlflow.log_metric("val accuracy", metric, epoch)
-        elif mode == 'regression':
-            for i in range(len(metric)):
-                mlflow.log_metric(f"val metric {i}",metric[i], epoch)
+        mlflow.log_metric("val accuracy", metric, epoch)
         val_loss.append(loss)
         val_metric.append(metric)
         scheduler.step(val_loss[-1])
@@ -75,7 +66,7 @@ def training(model, params):
 
 
 # calculate the loss per epochs
-def loss_epoch(model, loss_func, dataset_dl, epoch, fold, mode, sanity_check=False, opt=None, df=None):
+def classification_epoch(model, loss_func, dataset_dl, epoch, fold, mode, sanity_check=False, opt=None, df=None):
     running_loss = 0.0
     running_metrics = 0.0
     len_data = len(dataset_dl.sampler)
@@ -87,14 +78,43 @@ def loss_epoch(model, loss_func, dataset_dl, epoch, fold, mode, sanity_check=Fal
         xb = xb.to(device)
         yb = yb.to(device)
         output = model(xb)
-        
-        if mode == 'classification':
-            loss_b, metric_b = classification(loss_func, output, yb, name_b, conf_label, conf_pred, opt, df)
-        elif mode == 'regression':
-            loss_b, metric_b = regression(loss_func, output, yb, opt)
+        loss_b = loss_func(output, yb)
+        scores, pred_b = torch.max(output.data,1)
+        metric_b = (pred_b == yb).sum().item()
+    
+        if opt is not None:
+            opt.zero_grad()
+            loss_b.backward()
+            opt.step()
+    
+        if opt is None:
+            # Confusion Matrix에 쓰일 data append하는 부분
+            predictions_conv = pred_b.cpu().numpy()
+            labels_conv = yb.cpu().numpy()
+            conf_pred.append(predictions_conv)
+            conf_label.append(labels_conv)
+
+            if df is not None:
+                # 틀린 이미지 csv로 저장하는 부분
+                index = torch.nonzero((pred_b != yb)).squeeze().cpu().tolist()
+                if not isinstance(index, list):
+                    index = [index]  # index가 단일 값인 경우에 리스트로 변환하여 처리
+                scores = scores.cpu().numpy()
+                output = list(output.detach().cpu().numpy())
+                name_b = list(name_b)
+                for i in index:
+                    data = {'file_name':name_b[i], 
+                            '1++':output[i][0],
+                            '1+':output[i][1],
+                            '1':output[i][2],
+                            '2':output[i][3],
+                            '3':output[i][4], 
+                            'score':scores[i], 
+                            'prediction':predictions_conv[i]}
+                    new_row = pd.DataFrame(data=data, index=['file_name'])
+                    df = pd.concat([df,new_row], ignore_index=True)
 
         running_loss += loss_b.item()
-
         if metric_b is not None:
             running_metrics += metric_b
 
@@ -114,55 +134,109 @@ def loss_epoch(model, loss_func, dataset_dl, epoch, fold, mode, sanity_check=Fal
     return loss, metric, df
 
 
+def regression(model, params):
+    num_epochs=params['num_epochs']
+    loss_func=params['loss_func']
+    optimizer=params['optimizer']
+    scheduler=params['lr_scheduler']
+    log_epoch=params['log_epoch']
+    train_dl=params['train_dl']
+    val_dl=params['val_dl']
+    fold=params['fold']
+    sanity=params['sanity_check']
+    signature=params['signature']
+    num_classes=params['num_classes']
 
-def classification(loss_func, output, yb, name_b, conf_pred, conf_label, opt=None, df=None):
-    loss_b = loss_func(output, yb)
-    scores, pred_b = torch.max(output.data,1)
-    metric_b = (pred_b == yb).sum().item()
-    
-    if opt is not None:
-        opt.zero_grad()
-        loss_b.backward()
-        opt.step()
-    
-    if opt is None:
-        # Confusion Matrix에 쓰일 data append하는 부분
-        predictions_conv = pred_b.cpu().numpy()
-        labels_conv = yb.cpu().numpy()
-        conf_pred.append(predictions_conv)
-        conf_label.append(labels_conv)
+    train_loss, val_loss, train_metric, val_metric =[], [], [], []
+    best_loss = -1.0
+    for epoch in tqdm(range(num_epochs)):
+        
+        #training
+        model.train()
+        loss, metric = regression_epoch(model, loss_func, train_dl, epoch, fold+1, num_classes, sanity, optimizer)
+        
+        mlflow.log_metric("train loss", loss, epoch)
+        for i in range(num_classes):
+            mlflow.log_metric(f"train metric {i}",metric[i], epoch)
+        train_loss.append(loss)
+        train_metric.append(metric)
 
-        if df is not None:
-            # 틀린 이미지 csv로 저장하는 부분
-            index = torch.nonzero((pred_b != yb)).squeeze().cpu().tolist()
-            if not isinstance(index, list):
-                index = [index]  # index가 단일 값인 경우에 리스트로 변환하여 처리
-            scores = scores.cpu().numpy()
+        #validation
+        model.eval()
+        with torch.no_grad():
+            loss, metric = regression_epoch(model, loss_func, val_dl, epoch, fold+1, num_classes, sanity)
+        mlflow.log_metric("val loss", loss, epoch)
+
+        for i in range(num_classes):
+            mlflow.log_metric(f"val metric {i}",metric[i], epoch)
+        val_loss.append(loss)
+        val_metric.append(metric)
+        scheduler.step(val_loss[-1])
+
+        if epoch % log_epoch == log_epoch-1:
+            mlflow.pytorch.log_model(model, f'model_fold_{fold+1}_epoch_{epoch}', signature=signature)
+            
+        #saving best model
+        if sum(val_metric[-1])<best_loss or best_loss<0.0:
+            best_loss = sum(val_metric[-1])
+            mlflow.set_tag("best", f'best at epoch {epoch}')
+            mlflow.pytorch.log_model(model, f"best", signature=signature)
+        print('The Validation Loss is {} and the validation accuracy is {}'.format(val_loss[-1],val_metric[-1]))
+        print('The Training Loss is {} and the training accuracy is {}'.format(train_loss[-1],train_metric[-1]))
+
+    return model, train_metric, val_metric, train_loss, val_loss
+
+
+# calculate the loss per epochs
+def regression_epoch(model, loss_func, dataset_dl, epoch, fold, num_classes, sanity_check=False, opt=None):
+    running_loss = 0.0
+    running_metrics = np.zeros(num_classes)
+    len_data = len(dataset_dl.sampler)
+    df = pd.DataFrame(columns=['file_name','predict1','predict2','regression1','regression2'])
+    for xb, yb, name_b in tqdm(dataset_dl):
+        xb = xb.to(device)
+        yb = yb.to(device)
+        output = model(xb)
+        
+        metric_b = np.zeros(num_classes)
+        total_loss = 0.0
+        for i in range(num_classes):
+            loss_b = loss_func(output[:, i], yb[:, i])
+            total_loss += loss_b
+            metric_b[i] += loss_b.item()
+
+        if opt is not None:
+            opt.zero_grad()
+            total_loss.backward()
+            opt.step()
+            
+            # validation값 저장하는 부분
             output = list(output.detach().cpu().numpy())
+            yb = list(yb.cpu().numpy())
             name_b = list(name_b)
-            for i in index:
-                data = {'file_name':name_b[i], 
-                        '1++':output[i][0],
-                        '1+':output[i][1],
-                        '1':output[i][2],
-                        '2':output[i][3],
-                        '3':output[i][4], 
-                        'score':scores[i], 
-                        'prediction':predictions_conv[i]}
+            for i in range(len(output)):
+                data = {'file_name':name_b[i],
+                        'predict1':output[i][0], 
+                        'predict2':output[i][1],
+                        'regression1':yb[i][0],
+                        'regression2':yb[i][1],
+                       }
                 new_row = pd.DataFrame(data=data, index=['file_name'])
                 df = pd.concat([df,new_row], ignore_index=True)
-    return loss_b, metric_b
 
-def regression(loss_func, output, yb, opt=None):
-    losses_b = []
-    for i in range(output[0].shape):
-        loss_b = loss_func(output[:, i], yb[:, i])
-        losses_b.append(loss_b)
-    total_loss = sum(losses_b)
-    if opt is not None:
-        opt.zero_grad()
-        total_loss.backward()
-        opt.step()
+            if not os.path.exists('temp'):
+                os.mkdir('temp')
+            df.to_csv('temp/last_data.csv')
+            mlflow.log_artifact('temp/last_data.csv', f'output_epoch_{epoch}')
 
-    return total_loss, losses_b
-            
+        running_loss += total_loss.item()
+        if metric_b is not None:
+            running_metrics += metric_b
+
+        if sanity_check is True:
+            break
+
+    loss = running_loss / len_data
+    metric = running_metrics / len_data
+    return loss, metric
+
