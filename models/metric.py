0990a3042cacc304a43d3223dc06e6ad52d98e43
tool7831 <tool7831@gmail.com>
2023-09-23 16:57:56 +0900

    metric 함수 refactoring 진행. 아직 미완. 1. metric함수들 클래스로 묶음. 2. 아직 사용자의 입력을 받아서 동적으로 metric을 선택하는 부분은 안 만듦. 작동 테스트 하고 만들 예정

--- models/metric.py
+++ models/metric.py
@@ -1,21 +1,85 @@
 from torch import nn
 import torch
+import numpy as np
 
 class Accuracy():
-    def __init__(self, length):
-        self.cumulative_metric = 0.0
+    def __init__(self, length, num_classes, mode):
+        
+        self.num_classes = num_classes
         self.length = length
+        self.mode = mode
+
+        self.cumulative_metric = np.zeros(num_classes)
 
     def update(self, output, yb):
-        _, pred_b = torch.max(output.data,1)
-        metric_b = (pred_b == yb).sum().item()
-        self.cumulative_metric += metric_b
+        if self.mode == 'classification':
+            _, pred_b = torch.max(output.data,1)
+            metric_b = (pred_b == yb).sum().item()
+            self.cumulative_metric += metric_b
+
+        elif self.mode == 'regression':
+            if self.num_classes != 1:
+                for i in range(self.num_classes):
+                    self.cumulative_metric[i] += (torch.round(output[:,i]) == yb[:,i]).sum().item()
+            else:
+                self.cumulative_metric += (torch.round(output) == yb).sum().item()
         
     def getResult(self):
         return self.cumulative_metric / self.length
     
     def getClassName():
         return "Accuracy"
+    
+
+class R2score():
+    def __init__(self, length):
+        self.length = length
+        self.cumulative_y = None
+        self.cumulative_output = None
+    
+    def update(self, output, yb):
+        output = output.detach().cpu().numpy()
+        yb = yb.cpu().numpy()
+        if self.cumulative_y is None:
+            self.cumulative_y = np.array(yb)
+        else:
+            self.cumulative_y = np.vstack((self.cumulative_y,yb))
+            
+        if self.cumulative_output is None:
+            self.cumulative_output = np.array(output)
+        else:
+            self.cumulative_output = np.vstack((self.cumulative_output,output))
+
+    def getResult(self):
+        y_mean = self.cumulative_y.mean(axis=0)
+        ssr = np.square(self.cumulative_y - self.cumulative_output).sum(axis=0)
+        sst = np.square(self.cumulative_y - y_mean).sum(axis=0)
+        r2_score = (1 - (ssr / sst)).mean()
+        return r2_score
+    
+    def getClassName():
+        return "R2score"
+    
+
+
+class MeanAbsError():
+    def __init__(self, length, num_classes):
+        self.num_classes = num_classes
+        self.cumulative_metric = np.zeros(num_classes)
+        self.length = length
+    
+    def update(self, output, yb):
+        if self.num_classes != 1:
+            for i in range(self.num_classes):
+                self.cumulative_metric[i] += torch.abs(output[:,i] - yb[:,i]).sum().item()
+        else:
+            self.cumulative_metric += torch.abs(output - yb).sum().item()
+
+    def getResult(self):
+        return self.cumulative_metric / self.length
+    
+    def getClassName():
+        return "MAE"
 
 
 
--- models/train.py
+++ models/train.py
@@ -3,10 +3,9 @@ import mlflow
 from tqdm import tqdm
 import utils.analyze as analyze
 import numpy as np
-import pandas as pd
-import os
 from torch import nn
 import metric as f
+import utils.analyze_regression as analyze_r
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -64,10 +63,9 @@ def classification_epoch(model, loss_func, dataset_dl, epoch, columns_name, sani
 
     incorrect_output = analyze.IncorrectOutput(columns_name=["1++","1+","1","2","3"])
     confusion_matrix = analyze.ConfusionMatrix()
-    accuracy = f.Accuracy(len_data)
+    accuracy = f.Accuracy(len_data, 1, "classification")
 
     for xb, yb, name_b in tqdm(dataset_dl):
-        xb = xb
         yb = yb.to(device).long()
         yb = yb[:,0]
         output = model(xb)
@@ -125,30 +123,30 @@ def regression(model, params):
         
         #training
         model.train()
-        loss, acc, _, mae = regression_epoch(model, loss_func, train_dl, epoch, num_classes, columns_name, optimizer)
+        loss, metrics = regression_epoch(model, loss_func, train_dl, epoch, num_classes, columns_name, optimizer)
         
         mlflow.log_metric("train loss", loss, epoch)
         for i in range(num_classes):
-            mlflow.log_metric(f"train accuracy {columns_name[i]}",acc[i], epoch)
-            mlflow.log_metric(f"train mae {columns_name[i]}",mae[i], epoch)
+            mlflow.log_metric(f"train accuracy {columns_name[i]}",metrics[2][i], epoch)
+            mlflow.log_metric(f"train mae {columns_name[i]}",metrics[1][i], epoch)
         train_loss.append(loss)
-        train_acc.append(acc)
-        train_mae.append(mae)
+        train_acc.append(metrics[2])
+        train_mae.append(metrics[1])
 
         #validation
         model.eval()
         with torch.no_grad():
-            loss, acc, r2_score, mae = regression_epoch(model, loss_func, val_dl, epoch, num_classes, columns_name)
+            loss, metrics = regression_epoch(model, loss_func, val_dl, epoch, num_classes, columns_name)
 
-        mlflow.log_metric('r2 score',r2_score, epoch)
+        mlflow.log_metric('r2 score',metrics[0], epoch)
         mlflow.log_metric("val loss", loss, epoch)
         for i in range(num_classes):
-            mlflow.log_metric(f"val accuracy {columns_name[i]}",acc[i], epoch)
-            mlflow.log_metric(f"val mae {columns_name[i]}",mae[i], epoch)
+            mlflow.log_metric(f"val accuracy {columns_name[i]}",metrics[2][i], epoch)
+            mlflow.log_metric(f"val mae {columns_name[i]}",metrics[1][i], epoch)
         val_loss.append(loss)
-        val_acc.append(acc)
-        r2_list.append(r2_score)
-        val_mae.append(mae)
+        val_acc.append(metrics[2])
+        r2_list.append(metrics[0])
+        val_mae.append(metrics[1])
         scheduler.step(val_loss[-1])
 
         if epoch % log_epoch == log_epoch-1:
@@ -159,10 +157,11 @@ def regression(model, params):
             best_loss = val_loss[-1]
             mlflow.set_tag("best", f'best at epoch {epoch}')
             mlflow.pytorch.log_model(model, f"best")
+            
         print('The Validation Loss is {} and the Validation Accuracy is {}'.format(val_loss[-1],val_acc[-1]))
         print('The Training Loss is {} and the Training Accuracy is {}'.format(train_loss[-1],train_acc[-1]))
         print('The Training MAE is {} and the Validation MAE is {}'.format(train_mae[-1],val_mae[-1]))
-        print('The R2 score(fixed) is {}'.format(r2_score))
+        print('The R2 score(fixed) is {}'.format(r2_list[-1]))
 
     return model, train_acc, val_acc, train_loss, val_loss, r2_list, train_mae, val_mae
 
@@ -170,26 +169,20 @@ def regression(model, params):
 # calculate the loss per epochs
 def regression_epoch(model, loss_func, dataset_dl, epoch, num_classes, columns_name, opt=None):
     running_loss = 0.0
-    running_mae = np.zeros(num_classes)
-    running_acc = np.zeros(num_classes)
-    running_y = None
-    running_output = None
     len_data = len(dataset_dl.sampler)
-    r2_score =0.0
-    #-------------결과 저장할 data frame 정의하는 곳-------------
-    # 여기 바꾸면 아래 validation 저장하는 부분도 바꿔야함.
-    columns = ['file_name']
-    for i in range(num_classes):
-        columns.append('predict ' + columns_name[i])
-        columns.append('label ' + columns_name[i])
-    df = pd.DataFrame(columns=columns)
-    #----------------------------------------------------------
+
+    metrics=[]
+
+    accuracy = f.Accuracy(len_data, num_classes, 'regression')
+    r2_score = f.R2score(len_data)
+    mae = f.MeanAbsError(len_data, num_classes)
+
+    output_log = analyze_r.OutputLog(columns_name, num_classes)
 
     for xb, yb, name_b in tqdm(dataset_dl):
         yb = yb.to(device)
         output = model(xb)
         
-        metric_b = np.zeros(num_classes)
         total_loss = 0.0
         
         # class가 1개일 때 개별 라벨이 list 형식아니라서 for문을 못 돌림. 그래서 일단 구분함.
@@ -197,97 +190,30 @@ def regression_epoch(model, loss_func, dataset_dl, epoch, num_classes, columns_n
             for i in range(num_classes):
                 loss_b = loss_func(output[:, i], yb[:, i])
                 total_loss += loss_b
-                metric_b[i] += torch.abs(output[:,i] - yb[:,i]).sum().item()
-                running_acc[i] += (torch.round(output[:,i]) == yb[:,i]).sum().item()
         else:
             loss_b = loss_func(output, yb)
             total_loss += loss_b
-            metric_b += torch.abs(output - yb).sum().item()
-            running_acc += (torch.round(output) == yb).sum().item()
 
         running_loss += total_loss.item()
-        running_mae += metric_b
+        output_log.updateOutputLog(output, yb, name_b)
+
+        mae.update(output, yb)
+        accuracy.update(output, yb)
+        r2_score.update(output, yb)
+
 
         if opt is not None:
             opt.zero_grad()
             total_loss.backward()
-            opt.step()
-            
-            #---------------- 데이터 분석용 임시 추가-------------------------
-            output = output.detach().cpu().numpy()
-            yb = yb.cpu().numpy()
-
-            output = list(output)
-            yb = list(yb)
-            name_b = list(name_b)
-            for i in range(len(output)):
-                data = {'file_name':name_b[i]}
-                # class 개수 1개면 문제 생겨서 나눔.
-                if num_classes != 1:
-                    for j in range(num_classes):
-                        data['predict ' + columns_name[j]] = output[i][j]
-                        data['label ' + columns_name[j]] = yb[i][j]
-                else:
-                    data['predict ' + columns_name[0]] = output[i]
-                    data['label ' + columns_name[0]] = yb[i]
-                new_row = pd.DataFrame(data=data, index=['file_name'])
-                df = pd.concat([df,new_row], ignore_index=True)
-            #-----------------------------------------------------------------
+            opt.step()        
         
-        
-        if opt is None:
-            #-------------------- validation값 저장하는 부분 -------------------------
-            # 여기 바꾸면 위에 data frame 정의하는 곳도 바꿔야함. 
-            output = output.detach().cpu().numpy()
-            yb = yb.cpu().numpy()
-            
-            if running_y is None:
-                running_y = np.array(yb)
-            else:
-                running_y = np.vstack((running_y,yb))
-                
-            if running_output is None:
-                running_output = np.array(output)
-            else:
-                running_output = np.vstack((running_output,output))
-            
-            output = list(output)
-            yb = list(yb)
-            name_b = list(name_b)
-            for i in range(len(output)):
-                data = {'file_name':name_b[i]}
-                # class 개수 1개면 문제 생겨서 나눔.
-                if num_classes != 1:
-                    for j in range(num_classes):
-                        data['predict ' + columns_name[j]] = output[i][j]
-                        data['label ' + columns_name[j]] = yb[i][j]
-                else:
-                    data['predict ' + columns_name[0]] = output[i]
-                    data['label ' + columns_name[0]] = yb[i]
-                new_row = pd.DataFrame(data=data, index=['file_name'])
-                df = pd.concat([df,new_row], ignore_index=True)
-            #------------------------------------------------------------------------
-    
-    if opt is None:
-        y_mean = running_y.mean(axis=0)
-        ssr = np.square(running_y - running_output).sum(axis=0)
-        sst = np.square(running_y - y_mean).sum(axis=0)
-        r2_score = (1 - (ssr / sst)).mean()
-
-        if not os.path.exists('temp'):
-            os.mkdir('temp')
-        df.to_csv('temp/valid_output_data.csv')
-        mlflow.log_artifact('temp/valid_output_data.csv', f'output_epoch_{epoch}')
-        
-    if opt is not None:
-        if not os.path.exists('temp'):
-            os.mkdir('temp')
-        df.to_csv('temp/train_output_data.csv')
-        mlflow.log_artifact('temp/train_output_data.csv', f'output_epoch_{epoch}')
+    metrics.append(r2_score.getResult())
+    metrics.append(mae.getResult())
+    metrics.append(accuracy.getResult())
+    output_log.saveOutputLog(epoch, opt)
+ 
 
     loss = running_loss / len_data
-    mae = running_mae / len_data
-    accuracy = running_acc / len_data
-    return loss, accuracy, r2_score, mae
+    return loss, metrics
 
 
