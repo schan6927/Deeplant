a88810ee0bf9a8766b9f8d728cc673ffe55a8865
HoodBlack <102908971+HoodBlack@users.noreply.github.com>
2023-09-24 16:20:40 +0900

    Add files via upload

---
+++ models/utils/CM (1).py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import mlflow
+import seaborn as sns
+
+class SaveCM:
+    def __init__(self,con_mat,epoch):
+        self.con_mat=con_mat
+        self.epoch=epoch
+        
+    def save_plot(self,fold):    
+        cfs=sns.heatmap(self.con_mat,annot=True)
+        cfs.set(title='Confusion Matrix', ylabel='True lable', xlabel='Predict label')
+        figure = cfs.get_figure()
+        
+        # Save the plot as an image
+        mlflow.log_figure(figure, "Confusion_Matrix_"+str(fold)+'_'+str(self.epoch)+'.jpg')
+        
+        #close figure
+        plt.clf()
