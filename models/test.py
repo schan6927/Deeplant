86dfbd9d1d9a064821d813f7d33ad0a009f13f2f
schan6927 <106944065+schan6927@users.noreply.github.com>
2023-09-26 13:40:48 +0900

    comment update

--- models/test.py
+++ models/test.py
@@ -1,7 +1,7 @@
 import torch
 import mlflow
 from tqdm import tqdm
-import utils.confusion_matrix as cm
+import models.utils.confusion_matrix as cm
 import numpy as np
 import pandas as pd
 import os
@@ -11,6 +11,15 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def classification(model, params):
+    """    
+    모델을 평가모드로 전환시킨 후, (loss, metric, df)를 classification_epoch()를 통해 구한다.
+    그 후, loss 와 metric을 각각 test_loss 와 test_metric에 추가해준 후, test_loss 와 test_metric 을 반환한다. 
+    
+    (
+    model: 사용할 모델
+    ,params: 실험을 진행할 때 주어진 명령들의 집합
+    )
+    """
     num_epochs=params['num_epochs']
     test_dl=params['test_dl']
     sanity=params['sanity_check']
@@ -36,6 +45,23 @@ def classification(model, params):
         return model, test_metric, test_loss
 
 def classification_epoch(model, loss_func, dataset_dl, epoch, sanity_check=False, df=None):
+    """
+    학습을 거친 후 손실값, 정확도를 반환하고, 혼동행렬을 작성한다.
+    
+    모델에 입력값을 넣은 후, 손실함수에 결과값과 원본 데이터를 넣은 값인 loss 값을 모두 더한 후, 데이터의 길이 만큼 나눈 값을 loss에 저장한다.
+    또한 실제값과 결과값 사이에서 정확히 예측한 수를 metric에 더한후, 데이터의 길이 만큼 나눈 값을 metric에 저장한다.
+    결과 값을 Confusion Matrix에 쓰일 결과값과 예측값을 SaveCM()를 통해 confusion matrix를 작성한다.
+    최종적으로 구해진 loss, metric, df를 반환한다.
+    
+    (
+    model: 사용할 모델
+    ,loss_func: 사용할 손실 함수
+    ,dataset_dl: 사용할 데이터셋의 데이터로더
+    ,epoch: 현재 학습 횟수
+    ,sanity_check: 결함 여부 나타내는 flag
+    ,df: 실험에 따라 추가할 데이터 프레임(default = None)
+    )
+    """
     running_loss = 0.0
     running_metrics = 0.0
     len_data = len(dataset_dl.sampler)
@@ -97,6 +123,16 @@ def classification_epoch(model, loss_func, dataset_dl, epoch, sanity_check=False
 
 
 def regression(model, params):
+    """    
+    모델을 평가모드로 전환시킨 후, (loss, metric, df)를 classification_epoch()를 통해 구한다.
+    그 후, loss 와 metric을 각각 test_loss 와 test_metric에 추가해준 후, test_loss 와 test_metric 을 반환한다. 
+    
+    (
+    model: 사용할 모델
+    ,params: 실험을 진행할 때 주어진 명령들의 집합
+    )
+    """
+    
     num_epochs=params['num_epochs']
     loss_func=params['loss_func']
     test_dl=params['test_dl']
@@ -126,6 +162,26 @@ def regression(model, params):
 
 # calculate the loss per epochs
 def regression_epoch(model, loss_func, dataset_dl, epoch, num_classes, columns_name, sanity_check=False):
+    """
+    학습을 거친 후 손실값, 정확도를 반환하고, 혼동행렬을 작성한다.
+    
+    모델에 입력값을 넣은 후, 손실함수에 결과값과 원본 데이터를 넣은 값인 loss 값을 모두 더한 후, 데이터의 길이 만큼 나눈 값을 loss에 저장한다.
+    또한 실제값과 결과값 사이에서 정확히 예측한 수를 metric에 더한후, 데이터의 길이 만큼 나눈 값을 metric에 저장한다.
+    결과 값을 Confusion Matrix에 쓰일 결과값과 예측값을 SaveCM()를 통해 confusion matrix를 작성한다.
+    최종적으로 구해진 loss, metric, df를 반환한다.
+    
+    (
+    model: 사용할 모델
+    ,loss_func: 사용할 손실 함수
+    ,dataset_dl: 사용할 데이터셋의 데이터로더
+    ,epoch: 현재 학습 횟수
+    ,num_classes: 데이터의 클래스의 개수
+    ,columns_name: 각 클래스 컬럼의 이름
+    ,sanity_check: 결함 여부 나타내는 flag
+    ,df: 실험에 따라 추가할 데이터 프레임(default = None)
+    )
+    """
+    
     running_loss = 0.0
     running_metrics = np.zeros(num_classes)
     running_y = None
